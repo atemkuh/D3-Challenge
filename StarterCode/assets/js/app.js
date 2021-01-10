ba5e8f3bddd997d8a5adfd19b506507c79a029f9
joseph <>
2021-01-10 08:48:33 -0500

    link data, and format data to num values

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -7,3 +7,22 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 //setup SVG wrapper the append group to hold charts 
+var svg = d3
+    .select ("scatter")
+    .append("svg")
+    .attr("width",svgWidth)
+    .attr("height",svgHeight);
+
+var chartGroup = svg.append("g")
+    .attr("transform", 'translate(${margin.left}, ${margin.top})');
+
+//link data, and format data to num values
+d3.csv("assets/data/data.csv").then(function(riskData) {
+    riskData.forEach(function(data) {
+        data.age = +data.age;
+        data.smokes = +data.smokes;
+        data.healthcare = +data.healthcare;
+        data.poverty = +data.poverty;
+        data.abbr = data.abbr;
+        data.income = +data.income;
+    });
